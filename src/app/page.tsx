28e5f4ed3431772d44fbba7847d574a9ecf3c17a
rodrigo1163 <rodrigo.sena1163@gmail.com>
2025-06-23 08:25:43 -0400

    remove unused metadata from HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
 import { LandingPage } from "@/templates/lading-page";
-import { Metadata } from "next";
-
-export const matadata: Metadata = {
-  title: "Site.Set",
-  description: "Venda seus produtos como afiliado em um único lugar",
-  robots: "index, follow",
-};
 
 export default function HomePage() {
   return <LandingPage />;
